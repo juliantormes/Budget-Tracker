7356e8cf2bd2cb0205400f03e029ff04a29c31c3
Julian Tormes <juliants10@gmail.com>
2024-02-28 19:02:52 -0300

    Template now works as intended

--- budget_tracker/budget_tracker/urls.py
+++ budget_tracker/budget_tracker/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
+from tracker.views import expense_list  # Import the view
 
 urlpatterns = [
     path('tracker/', include('tracker.urls')),
+    path('', expense_list, name='home'),  # Use expense_list for the root URL
 ]

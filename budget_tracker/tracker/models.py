2797e90211bf541b7ddf356832896118eaa203c6
Julian Tormes <juliants10@gmail.com>
2024-02-29 21:03:19 -0300

    Added forms

--- budget_tracker/tracker/models.py
+++ budget_tracker/tracker/models.py
@@ -3,8 +3,14 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Expense(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    description = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, related_name='expenses', on_delete=models.CASCADE)
+    description = models.CharField(max_length=255, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
+
+    def __str__(self):
+        return f"{self.category.name}: {self.amount} on {self.date}"

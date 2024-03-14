56cae50353624e292cbbad57ed9c32d1e586add9
Julian Tormes <juliants10@gmail.com>
2024-03-13 22:41:01 -0300

    Added helpers

--- budget_tracker/tracker/views.py
+++ budget_tracker/tracker/views.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from collections import defaultdict
 from decimal import Decimal, InvalidOperation
+from datetime import datetime,timedelta
 
 def signup(request):
     if request.method == 'POST':
@@ -48,6 +49,33 @@ def aggregate_data(queryset, label_field, value_field):
         value = entry[value_field]
         aggregated_data[label] += value  # Sum amounts with the same label
     return list(aggregated_data.items())
+def get_effective_month(date, close_card_day):
+    if date.day > close_card_day:
+        # Expense goes to the month after next month
+        effective_date = (date.replace(day=1) + timedelta(days=32)).replace(day=1)
+    else:
+        # Expense goes to the next month
+        effective_date = (date.replace(day=1) + timedelta(days=31)).replace(day=1)
+    return effective_date
+def calculate_total_payment_with_surcharge(amount, surcharge_percentage):
+    P = Decimal(amount)
+    S = Decimal(surcharge_percentage) / Decimal(100)
+    total_payment = P + (P * S)
+    return total_payment
+
+def distribute_installments(expense, close_card_day):
+    distributed_payments = defaultdict(list)
+    total_payment = calculate_total_payment_with_surcharge(expense.amount, expense.surcharge)
+    monthly_payment = total_payment / Decimal(expense.installments)
+
+    for i in range(expense.installments):
+        effective_date = expense.date + relativedelta(months=i)
+        if effective_date.day > close_card_day:
+            effective_date += relativedelta(months=1)
+        effective_month = effective_date.replace(day=1)
+        distributed_payments[effective_month].append(monthly_payment)
+
+    return distributed_payments
 
 @login_required
 def home(request):

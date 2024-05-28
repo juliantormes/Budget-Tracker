cfc0d517b4813b9922e848e54068a147e497423f
Julian Tormes <juliants10@gmail.com>
2024-05-28 04:08:39 -0300

    Fixed Logout and form styles

--- budget_tracker/my_budget_tracker_app/src/components/Header.js
+++ budget_tracker/my_budget_tracker_app/src/components/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import LogoutIcon from '@mui/icons-material/Logout';
 
-const Header = ({ logout }) => (
-    <div className="homepage-header">
-        <div className="header-left">
-            <span className="header-title">Budget Tracker</span>
-        </div>
-        <button className="logout-button" onClick={logout}>
-            <LogoutIcon /> Logout
-        </button>
-    </div>
-);
+const Header = ({ logout }) => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate('/login'); // Redirect to the login page
+    };
+
+    return (
+        <header className="homepage-header">
+            <div className="header-left">
+                <span className="header-title">Budget Tracker</span>
+            </div>
+            <button className="logout-button" onClick={handleLogout}>
+                <LogoutIcon /> Logout
+            </button>
+        </header>
+    );
+};
 
 export default Header;

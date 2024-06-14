21432be7de1dabe5d73e9bf09aa3eb28420f3337
juliantormes <juliants10@gmail.com>
2024-06-14 00:43:35 -0300

    Keep track of sidebar submenus

--- budget_tracker/my_budget_tracker_app/src/components/SidebarMenu.js
+++ budget_tracker/my_budget_tracker_app/src/components/SidebarMenu.js
@@ -1,52 +1,102 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Sidebar as ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import CreditCardIcon from '@mui/icons-material/CreditCard';
 import PieChartIcon from '@mui/icons-material/PieChart';
 import ExpenseIcon from '@mui/icons-material/Receipt';
 import IncomeIcon from '@mui/icons-material/AttachMoney';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const SidebarMenu = () => (
+const SidebarMenu = () => {
+  const location = useLocation();
+  const [expandedMenus, setExpandedMenus] = useState({
+    incomes: false,
+    expenses: false,
+    creditCard: false,
+    graphics: false,
+  });
+
+  useEffect(() => {
+    const path = location.pathname;
+    if (path.includes('income')) {
+      setExpandedMenus((prevState) => ({ ...prevState, incomes: true }));
+    } else if (path.includes('expense')) {
+      setExpandedMenus((prevState) => ({ ...prevState, expenses: true }));
+    } else if (path.includes('credit-card')) {
+      setExpandedMenus((prevState) => ({ ...prevState, creditCard: true }));
+    } else if (path.includes('graphics')) {
+      setExpandedMenus((prevState) => ({ ...prevState, graphics: true }));
+    }
+  }, [location.pathname]);
+
+  const handleToggle = (menu) => {
+    setExpandedMenus((prevState) => ({
+      ...prevState,
+      [menu]: !prevState[menu],
+    }));
+  };
+
+  return (
     <ProSidebar
-        rootStyles={{
-            backgroundColor: '#1f2a40',
-            borderRight: 'none',
-        }}
+      rootStyles={{
+        backgroundColor: '#1f2a40',
+        borderRight: 'none',
+      }}
     >
-        <Menu
-            menuItemStyles={{
-                button: ({ level, active }) => ({
-                    color: '#e0e6ed', // Change text color to match your app
-                    backgroundColor: active ? '#2c3e50' : '#1f2a40', // Change background color for active and inactive states
-                    '&:hover': {
-                        backgroundColor: '#2c3e50', // Change hover background color
-                        color: '#4a90e2', // Change hover text color to match your app
-                    },
-                }),
-            }}
+      <Menu
+        menuItemStyles={{
+          button: ({ level, active }) => ({
+            color: '#e0e6ed', // Change text color to match your app
+            backgroundColor: active ? '#2c3e50' : '#1f2a40', // Change background color for active and inactive states
+            '&:hover': {
+              backgroundColor: '#2c3e50', // Change hover background color
+              color: '#4a90e2', // Change hover text color to match your app
+            },
+          }),
+        }}
+      >
+        <SubMenu
+          label="Incomes"
+          icon={<IncomeIcon />}
+          open={expandedMenus.incomes}
+          onOpenChange={() => handleToggle('incomes')}
         >
-            <SubMenu label="Incomes" icon={<IncomeIcon />}>
-                <MenuItem component={<Link to="/view-incomes" />}>View Incomes</MenuItem>
-                <MenuItem component={<Link to="/add-incomes" />}>Add Incomes</MenuItem>
-                <MenuItem component={<Link to="/view-income-category" />}>View Income Category</MenuItem>
-                <MenuItem component={<Link to="/view-income-category?add=true" />}>Add Income Category</MenuItem>
-            </SubMenu>
-            <SubMenu label="Expenses" icon={<ExpenseIcon />}>
-                <MenuItem component={<Link to="/view-expenses" />}>View Expenses</MenuItem>
-                <MenuItem component={<Link to="/add-expenses" />}>Add Expenses</MenuItem>
-                <MenuItem component={<Link to="/view-expense-category" />}>View Expense Category</MenuItem>
-                <MenuItem component={<Link to="/view-expense-category?add=true" />}>Add Expense Category</MenuItem>
-            </SubMenu>
-            <SubMenu label="Credit Card" icon={<CreditCardIcon />}>
-                <MenuItem component={<Link to="/view-credit-card" />}>View Credit Card</MenuItem>
-                <MenuItem component={<Link to="/add-credit-card" />}>Add Credit Card</MenuItem>
-            </SubMenu>
-            <SubMenu label="Graphics" icon={<PieChartIcon />}>
-                <MenuItem component={<Link to="/pie-graphics" />}>Pie Graphics</MenuItem>
-                <MenuItem component={<Link to="/bar-graphics" />}>Bar Graphics</MenuItem>
-            </SubMenu>
-        </Menu>
+          <MenuItem component={<Link to="/view-incomes" />}>View Incomes</MenuItem>
+          <MenuItem component={<Link to="/add-incomes" />}>Add Incomes</MenuItem>
+          <MenuItem component={<Link to="/view-income-category" />}>View Income Category</MenuItem>
+          <MenuItem component={<Link to="/view-income-category?add=true" />}>Add Income Category</MenuItem>
+        </SubMenu>
+        <SubMenu
+          label="Expenses"
+          icon={<ExpenseIcon />}
+          open={expandedMenus.expenses}
+          onOpenChange={() => handleToggle('expenses')}
+        >
+          <MenuItem component={<Link to="/view-expenses" />}>View Expenses</MenuItem>
+          <MenuItem component={<Link to="/add-expenses" />}>Add Expenses</MenuItem>
+          <MenuItem component={<Link to="/view-expense-category" />}>View Expense Category</MenuItem>
+          <MenuItem component={<Link to="/view-expense-category?add=true" />}>Add Expense Category</MenuItem>
+        </SubMenu>
+        <SubMenu
+          label="Credit Card"
+          icon={<CreditCardIcon />}
+          open={expandedMenus.creditCard}
+          onOpenChange={() => handleToggle('creditCard')}
+        >
+          <MenuItem component={<Link to="/view-credit-card" />}>View Credit Card</MenuItem>
+          <MenuItem component={<Link to="/add-credit-card" />}>Add Credit Card</MenuItem>
+        </SubMenu>
+        <SubMenu
+          label="Graphics"
+          icon={<PieChartIcon />}
+          open={expandedMenus.graphics}
+          onOpenChange={() => handleToggle('graphics')}
+        >
+          <MenuItem component={<Link to="/pie-graphics" />}>Pie Graphics</MenuItem>
+          <MenuItem component={<Link to="/bar-graphics" />}>Bar Graphics</MenuItem>
+        </SubMenu>
+      </Menu>
     </ProSidebar>
-);
+  );
+};
 
 export default SidebarMenu;

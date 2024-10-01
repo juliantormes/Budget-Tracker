cfffafd5e19b71e46b917b633edb47724ed18cca
juliantormes <juliants10@gmail.com>
2024-10-01 19:35:29 -0300

    proper config to automatically detect jest test

---
+++ budget_tracker/my_budget_tracker_app/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+    transform: {
+      '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+  };
+  

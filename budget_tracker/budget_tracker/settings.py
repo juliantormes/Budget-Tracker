1eb2ef6027c4c9167bf988eeee9d88f57ffcb912
juliantormes <juliants10@gmail.com>
2024-10-31 18:06:32 -0300

    CORS ADDED

--- budget_tracker/budget_tracker/settings.py
+++ budget_tracker/budget_tracker/settings.py
@@ -64,6 +64,11 @@ TEMPLATES = [
         },
     },
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React app's local address
+]
 
 WSGI_APPLICATION = 'budget_tracker.wsgi.application'
 

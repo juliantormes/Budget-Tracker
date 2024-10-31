011b1b08a4a6458bfbd1d60884da9a84da0a2c18
juliantormes <juliants10@gmail.com>
2024-10-31 18:12:00 -0300

    Added trusted origins

--- budget_tracker/budget_tracker/settings.py
+++ budget_tracker/budget_tracker/settings.py
@@ -69,6 +69,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React app's local address
 ]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+
 
 WSGI_APPLICATION = 'budget_tracker.wsgi.application'
 

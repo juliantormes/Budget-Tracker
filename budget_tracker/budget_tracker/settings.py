5874c3b9892d9ed23809d892f718324a140e2c04
juliantormes <juliants10@gmail.com>
2024-10-31 21:33:32 -0300

    Change in settings.py

--- budget_tracker/budget_tracker/settings.py
+++ budget_tracker/budget_tracker/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # Static files configuration
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

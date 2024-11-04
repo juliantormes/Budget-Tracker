a653d5f0474ebf5106f30bf05e69fb5f5910f730
juliantormes <juliants10@gmail.com>
2024-11-04 04:08:46 -0300

    Going back

--- budget_tracker/tracker/urls.py
+++ budget_tracker/tracker/urls.py
@@ -11,14 +11,13 @@ router.register(r'credit_cards', views.CreditCardViewSet)
 router.register(r'credit-card-expenses', views.CreditCardExpenseViewSet, basename='credit-card-expense')
 
 urlpatterns = [
-    # Prefix all API endpoints with 'api/v1/' for organization and to avoid exposing root.
-    path('api/v1/', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('login/', views.login, name='login'),  # Custom login view
-    path('signup/', views.signup, name='signup'),  # Custom signup view
-    path('logout/', views.logout, name='logout'),  # Custom logout view
-
+    path('login/', views.login, name='login'),  # Pointing to a custom login view
+    path('signup/', views.signup, name='signup'),  # Pointing to a custom signup view
+    path('logout/', views.logout, name='logout'),  # Pointing to a custom logout view
+    path('', include(router.urls)),
     # Custom routes for updating recurring incomes and expenses
-    path('api/v1/expenses/<int:expense_id>/update_recurring/', views.update_recurring_expense, name='update_recurring_expense'),
-    path('api/v1/incomes/<int:income_id>/update_recurring/', views.update_recurring_income, name='update_recurring_income'),
+    path('api/expenses/<int:expense_id>/update_recurring/', views.update_recurring_expense, name='update_recurring_expense'),
+    path('api/incomes/<int:income_id>/update_recurring/', views.update_recurring_income, name='update_recurring_income'),
 ]

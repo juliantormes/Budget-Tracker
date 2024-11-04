a5e9947e1350b645be383e9eb99c25f6e86c8c5e
juliantormes <juliants10@gmail.com>
2024-11-04 03:43:18 -0300

    Change middleware to restric api root too

--- budget_tracker/budget_tracker/middleware/restrict_api_middleware.py
+++ budget_tracker/budget_tracker/middleware/restrict_api_middleware.py
@@ -3,13 +3,13 @@ from django.http import HttpResponseForbidden
 
 class RestrictAPIAccessMiddleware:
     """
-    Middleware to restrict API access when DEBUG is False (production mode).
+    Middleware to restrict access to any endpoint starting with '/api-' when DEBUG is False (production mode).
     """
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         # Only restrict API access if DEBUG is False
-        if not settings.DEBUG and request.path.startswith('/api/'):
+        if not settings.DEBUG and request.path.startswith('/api-'):
             return HttpResponseForbidden("Access to API is restricted.")
         return self.get_response(request)

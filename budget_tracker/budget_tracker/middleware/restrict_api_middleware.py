4f22ca43f84dfabb916f4a7b4ac902ca4dfb65f2
juliantormes <juliants10@gmail.com>
2024-11-04 03:44:12 -0300

    Fixed ty[po

--- budget_tracker/budget_tracker/middleware/restrict_api_middleware.py
+++ budget_tracker/budget_tracker/middleware/restrict_api_middleware.py
@@ -3,13 +3,16 @@ from django.http import HttpResponseForbidden
 
 class RestrictAPIAccessMiddleware:
     """
-    Middleware to restrict access to any endpoint starting with '/api-' when DEBUG is False (production mode).
+    Middleware to restrict API access in production (when DEBUG is False).
     """
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         # Only restrict API access if DEBUG is False
-        if not settings.DEBUG and request.path.startswith('/api-'):
-            return HttpResponseForbidden("Access to API is restricted.")
+        if not settings.DEBUG:
+            restricted_paths = ['/api/', '/api-']
+            # Check if the request path starts with any restricted path
+            if any(request.path.startswith(path) for path in restricted_paths):
+                return HttpResponseForbidden("Access to API is restricted.")
         return self.get_response(request)

525cab8da5e27c8e77460f1abc03b863f100b50d
juliantormes <juliants10@gmail.com>
2024-11-04 03:53:54 -0300

    Changing api root paht

--- budget_tracker/tracker/urls.py
+++ budget_tracker/tracker/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
@@ -13,12 +14,20 @@ router.register(r'credit-card-expenses', views.CreditCardExpenseViewSet, basenam
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('login/', views.login, name='login'),  # Pointing to a custom login view
-    path('signup/', views.signup, name='signup'),  # Pointing to a custom signup view
-    path('logout/', views.logout, name='logout'),  # Pointing to a custom logout view
-    path('', include(router.urls)),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
     # Custom routes for updating recurring incomes and expenses
     path('api/expenses/<int:expense_id>/update_recurring/', views.update_recurring_expense, name='update_recurring_expense'),
     path('api/incomes/<int:income_id>/update_recurring/', views.update_recurring_income, name='update_recurring_income'),
-    
 ]
+
+# Add a separate API root path for production
+if not settings.DEBUG:
+    urlpatterns += [
+        path('api-root/', include(router.urls)),  # Access API root at /api-root in production
+    ]
+else:
+    urlpatterns += [
+        path('', include(router.urls)),  # Access API root at blank path in development
+    ]

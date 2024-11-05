30631a0f592e99c4d342ccd3a714a2b4a9d7a36e
juliantormes <juliants10@gmail.com>
2024-11-05 19:39:19 -0300

    Added simple router when debug = false

--- budget_tracker/tracker/urls.py
+++ budget_tracker/tracker/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
 from . import views
 
-router = DefaultRouter()
+# Conditionally set the router based on the DEBUG setting
+if settings.DEBUG:
+    router = DefaultRouter()
+else:
+    router = SimpleRouter()
+
+# Register your viewsets with the chosen router
 router.register(r'incomes', views.IncomeViewSet)
 router.register(r'expenses', views.ExpenseViewSet)
 router.register(r'expense_categories', views.ExpenseCategoryViewSet)
@@ -10,13 +17,14 @@ router.register(r'income_categories', views.IncomeCategoryViewSet)
 router.register(r'credit_cards', views.CreditCardViewSet)
 router.register(r'credit-card-expenses', views.CreditCardExpenseViewSet, basename='credit-card-expense')
 
+# Define urlpatterns with the selected router
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('login/', views.login, name='login'),  # Pointing to a custom login view
-    path('signup/', views.signup, name='signup'),  # Pointing to a custom signup view
-    path('logout/', views.logout, name='logout'),  # Pointing to a custom logout view
-    path('', include(router.urls)),
+    path('login/', views.login, name='login'),  # Custom login view
+    path('signup/', views.signup, name='signup'),  # Custom signup view
+    path('logout/', views.logout, name='logout'),  # Custom logout view
+
     # Custom routes for updating recurring incomes and expenses
     path('api/expenses/<int:expense_id>/update_recurring/', views.update_recurring_expense, name='update_recurring_expense'),
     path('api/incomes/<int:income_id>/update_recurring/', views.update_recurring_income, name='update_recurring_income'),
